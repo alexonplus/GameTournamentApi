4986d8ba7dfc8f5f7b261dbbe3d500284935078d
alexonplus <oleks9185@gmail.com>
2026-02-15 15:01:40 +0100

    Add project files.

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace GameTournamentApi.Models
+{
+    public class Game
+    {
+public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+       
+        public DateTime ReleaseDate { get; set; }
+        public int TournamentId { get; set; }
+        public Tournament Tournament { get; set; } = null!;
+
+
+    }
+}
---
+++ Models/Tournament.cs
@@ -0,0 +1,26 @@
+﻿namespace GameTournamentApi.Models
+{
+    public class Tournament
+    {
+        // Unique number. In the database, it will become the Primary Key.
+        public int Id { get; set; }
+       
+        // Tournament name. By default, we set it to an empty string to avoid errors.
+        public string Title { get; set; } = string.Empty;
+
+        //Description of what kind of tournament it is.
+
+        public string Description { get; set; } = string.Empty;
+
+        //How many players can participate in the tournament. 
+        public int MaxPlayers { get; set; }
+
+        //Date of the tournament. 
+        public DateTime Date { get; set; }
+
+
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+
+
+    }
+}
