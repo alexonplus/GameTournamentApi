e30b737cca673cbc76a02cb9fe1f367a2ba64bc0
alexonplus <oleks9185@gmail.com>
2026-02-20 20:11:08 +0100

    feat: implement tournament service and DI
    add TournamentService with interface
    
    setup service layer for tournaments

---
+++ Services/TournamentService.cs
@@ -0,0 +1,23 @@
+﻿using GameTournamentApi.Data;
+using GameTournamentApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameTournamentApi.Services
+{
+    public class TournamentService : ITournamentService
+    {
+        private readonly TournamentDbContext _context;
+
+        //// Constructor:  ask the system to give us access to the database
+        public TournamentService(TournamentDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Tournament>> GetAllTournamentsAsync()
+        {
+            return await _context.Tournaments.ToListAsync();
+        }
+
+    
+    }
+}
