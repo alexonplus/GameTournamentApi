1051fdfb253919c972a2cf8c32b0dc3c59c57866
alexonplus <oleks9185@gmail.com>
2026-02-21 15:42:11 +0100

    1.initial setup of swagger, models, and migrations
    2. update entities and reset database
    
    3.setup: tournament controller and db context

--- Models/Game.cs
+++ Models/Game.cs
@@ -5,7 +5,7 @@
 public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
        
-        public DateTime ReleaseDate { get; set; }
+        public DateTime Time { get; set; }
         public int TournamentId { get; set; }
         public Tournament Tournament { get; set; } = null!;
 
