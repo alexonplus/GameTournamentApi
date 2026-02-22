f8fd57273192bb45ddb09363595c8f3fa18a4072
alexonplus <oleks9185@gmail.com>
2026-02-22 12:35:42 +0100

    Add Tournament POST and Game DTOs

---
+++ DTOs/TournamentCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameTournamentApi.DTOs
+{
+    public class TournamentCreateDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int MaxPlayers { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
