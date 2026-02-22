114a06b3a1df721c8c9b04d5d193df431181d8be
alexonplus <oleks9185@gmail.com>
2026-02-22 20:07:24 +0100

    Refactor GameService to use GameDto and fix circular reference

--- DTOs/GameDto.cs
+++ DTOs/GameDto.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
+
+        // without this, we get a 500 circular reference error when trying to serialize the Game object.f****
+        public int TournamentId { get; set; }
     }
 }
---
+++ Services/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using GameTournamentApi.DTOs;
+
+namespace GameTournamentApi.Services
+{
+    public interface IGameService
+    {
+        // Returns a DTO to avoid circular reference errors
+        Task<GameDto?> AddGameAsync(GameCreateDto gameDto);
+
+        // Returns a list of games for a specific tournament
+        Task<IEnumerable<GameDto>> GetAllGamesAsync();
+    }
+}
